908487df56378288265ab381ea381809034d592a
Tobias Theuer <theuer@synyx.de>
2015-07-17 15:52:53 +0200

    Added PUT controller. Doesn't work though.

--- src/main/java/org/synyx/sybil/bricklet/output/ledstrip/api/DisplayController.java
+++ src/main/java/org/synyx/sybil/bricklet/output/ledstrip/api/DisplayController.java
@@ -9,17 +9,22 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.hateoas.Link;
 
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import org.synyx.sybil.LoadFailedException;
+import org.synyx.sybil.bricklet.output.ledstrip.Color;
 import org.synyx.sybil.bricklet.output.ledstrip.LEDStripDTOService;
 import org.synyx.sybil.bricklet.output.ledstrip.LEDStripService;
+import org.synyx.sybil.bricklet.output.ledstrip.Sprite1D;
 import org.synyx.sybil.bricklet.output.ledstrip.domain.LEDStripDTO;
 
 import java.io.IOException;
+
+import java.util.List;
 
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.linkTo;
 import static org.springframework.hateoas.mvc.ControllerLinkBuilder.methodOn;
@@ -70,5 +75,36 @@ public class DisplayController {
         }
 
         return displayResource;
+    }
+
+
+    @ResponseBody
+    @RequestMapping(method = RequestMethod.PUT, produces = "application/hal+json")
+    public DisplayResource putDisplay(@PathVariable String name, @RequestBody DisplayResource displayResource) {
+
+        List<Color> pixels = displayResource.getPixels();
+
+        if (pixels != null && !pixels.isEmpty()) {
+            Sprite1D sprite1D;
+            LEDStripDTO ledStripDTO;
+
+            try {
+                ledStripDTO = ledStripDTOService.getDTO(name);
+            } catch (IOException | NullPointerException exception) {
+                throw new LoadFailedException("Error loading LED strip:", exception);
+            }
+
+            sprite1D = new Sprite1D(pixels.size(), pixels);
+            ledStripDTO.setSprite(sprite1D);
+
+            try {
+                ledStripService.handleSprite(ledStripDTO);
+            } catch (TimeoutException | AlreadyConnectedException | IOException | NotConnectedException
+                    | NullPointerException exception) {
+                throw new LoadFailedException("Error setting pixels on LED strip:", exception);
+            }
+        }
+
+        return getDisplay(name);
     }
 }
